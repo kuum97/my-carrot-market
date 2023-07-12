82f0acbdf8ccd6600eaacbeefa6ec5e0561cb697
kuum97 <kuum97@icloud.com>
2023-07-12 16:13:43 +0900

    implement prisma token for post request

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -5,17 +5,23 @@ import client from "@/libs/server/client";
 async function handler(req: NextApiRequest, res: NextApiResponse) {
   const { phone, email } = req.body;
   const payload = phone ? { phone: +phone } : { email };
-  const user = await client.user.upsert({
-    where: {
-      ...payload,
+  const token = await client.token.create({
+    data: {
+      payload: "123",
+      user: {
+        connectOrCreate: {
+          where: {
+            ...payload,
+          },
+          create: {
+            name: "Anonymous",
+            ...payload,
+          },
+        },
+      },
     },
-    create: {
-      name: "Anonymous",
-      ...payload,
-    },
-    update: {},
   });
-  console.log(user);
+  console.log(token);
   /* if (email) {
     user = await client.user.findUnique({
       where: {
